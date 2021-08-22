f2c889fc1ba57f890a5b7f542d7e54b67ece99ae
BigRedEye <mail@bigredeye.me>
2021-08-22 04:27:50 +0300

    fix: Set -fconstexpr-steps for clang only

--- cmake/BuildFlags.cmake
+++ cmake/BuildFlags.cmake
@@ -14,8 +14,6 @@ else()
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -g")
 endif()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fconstexpr-steps=1271242")
-
 set(CMAKE_CXX_FLAGS_ASAN "-g -fsanitize=address,undefined -fno-sanitize-recover=all"
     CACHE STRING "Compiler flags in asan build"
     FORCE)
