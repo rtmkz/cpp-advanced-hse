b16e020cb5b9201dca53c0312876ac31ad5d45e5
Danila Kutenin <kutdanila@yandex.ru>
2021-10-29 12:26:02 +0100

    Better pipes

--- tasks/pipes/test.cpp
+++ tasks/pipes/test.cpp
@@ -5,14 +5,14 @@
 TEST_CASE("Simple pipeline") {
     auto f = [](int a) { return a + 2; };
     auto g = [](int a) { return std::make_pair(a, a > 3); };
-    auto pipe = empty_pl | f | g;
+    auto pipe = kEmptyPl | f | g;
 
     REQUIRE(pipe(0) == std::make_pair(2, false));
     REQUIRE(pipe(2) == std::make_pair(4, true));
 }
 
 TEST_CASE("Inline pipeline") {
-    auto pipeline = empty_pl | [](bool should_pass) {
+    auto pipeline = kEmptyPl | [](bool should_pass) {
         return std::make_tuple(should_pass, "hello world", nullptr);
     } | [](const auto& t) {
         return std::get<0>(t) ? std::get<1>(t) : std::get<2>(t);
