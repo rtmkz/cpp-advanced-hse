67c6181c1ea95579092549ebe364c239b69ead49
Danila Kutenin <kutdanila@yandex.ru>
2021-11-04 11:51:56 +0000

    Prepare repo to handle multithreading

--- cmake/BuildFlags.cmake
+++ cmake/BuildFlags.cmake
@@ -17,4 +17,8 @@ set(CMAKE_CXX_FLAGS_ASAN "-g -fsanitize=address,undefined -fno-sanitize-recover=
     CACHE STRING "Compiler flags in asan build"
     FORCE)
 
+set(CMAKE_CXX_FLAGS_TSAN "-g -fsanitize=thread -fno-sanitize-recover=all"
+  CACHE STRING "Compiler flags in tsan build"
+  FORCE)
+
 set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_ASAN} -fprofile-instr-generate -fcoverage-mapping")
