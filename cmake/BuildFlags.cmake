2dc5a6a94d5dd46b5c2d9e84f07c1103cf247ec9
Sergey Skvortsov <mail@bigredeye.me>
2021-11-06 15:23:03 +0300

    feat(cmake): Add MSAN build

--- cmake/BuildFlags.cmake
+++ cmake/BuildFlags.cmake
@@ -22,4 +22,8 @@ set(CMAKE_CXX_FLAGS_TSAN "-g -fsanitize=thread -fno-sanitize-recover=all"
   CACHE STRING "Compiler flags in tsan build"
   FORCE)
 
+set(CMAKE_CXX_FLAGS_MSAN "-g -fsanitize=memory -fsanitize-recover=all"
+  CACHE STRING "Compiler flags in msan build"
+  FORCE)
+
 set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_ASAN} -fprofile-instr-generate -fcoverage-mapping")
