99b05213b9a5a43c8ebef44a5cae209a477035b7
BigRedEye <mail@bigredeye.me>
2021-11-07 22:26:18 +0300

    refactor: Counter as template parameter

--- seminars/08-threading/06-false-sharing.cpp
+++ seminars/08-threading/06-false-sharing.cpp
@@ -10,21 +10,8 @@
 constexpr unsigned kLimit = 10'000'000;
 constexpr unsigned kNumThreads = 12;
 
-struct Counter {
-public:
-    void Increment() {
-        value_ = value_ + 1;
-    }
-
-    unsigned Value() {
-        return value_;
-    }
-
-private:
-    std::atomic<unsigned> value_ = 0;
-};
-
-void Slow() {
+template <typename Counter>
+void Run() {
     timer::Timer timer;
 
     std::array<Counter, kNumThreads> counters{};
@@ -50,44 +37,32 @@ void Slow() {
     std::cout << "elapsed: " << timer.Elapsed() << std::endl;
 }
 
-template <typename T, size_t Alignment>
-struct alignas(Alignment) AlignedValue : public T {
+struct Counter {
+public:
+    void Increment() {
+        value_ = value_ + 1;
+    }
+
+    unsigned Value() {
+        return value_;
+    }
+
+private:
+    std::atomic<unsigned> value_ = 0;
 };
 
 constexpr size_t kCacheLineSize = 64;
 
-void Fast() {
-    timer::Timer timer;
+template <typename T, size_t Alignment>
+struct alignas(Alignment) AlignedValue : public T {};
 
-    unsigned num_threads = kNumThreads;
-    std::array<AlignedValue<Counter, kCacheLineSize>, kNumThreads> counters{};
-
-    std::vector<std::thread> workers;
-
-    for (unsigned i = 0; i < num_threads; ++i) {
-        workers.emplace_back([i, &counters] {
-            for (size_t pos = 0; pos < kLimit; ++pos) {
-                counters[i].Increment();
-            }
-        });
-    }
-
-    for (std::thread& t : workers) {
-        t.join();
-    }
-
-    for (auto& counter : counters) {
-        assert(counter.Value() == kLimit);
-    }
-
-    std::cout << "elapsed: " << timer.Elapsed() << std::endl;
-}
+using AlignedCounter = AlignedValue<Counter, kCacheLineSize>;
 
 int main() {
     for (int rep = 0; rep < 3; ++rep) {
         std::cout << "slow ";
-        Slow();
+        Run<Counter>();
         std::cout << "fast ";
-        Fast();
+        Run<AlignedCounter>();
     }
 }
