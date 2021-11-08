63a1c2a7069fca96df1558c7b94950048d871b3f
Danila Kutenin <kutdanila@yandex.ru>
2021-11-08 17:33:01 +0000

    Fix run

--- tasks/reduce/run.cpp
+++ tasks/reduce/run.cpp
@@ -11,7 +11,7 @@ uint32_t Gcd(uint32_t a, uint32_t b) {
     return !b ? a : Gcd(b, a % b);
 }
 
-const std::vector<uint32_t> kTest(GenTest(kMaxSize));
+const std::vector<uint32_t> kTest(GenTest<uint32_t>(kMaxSize));
 const uint32_t kOkResult = std::accumulate(kTest.begin(), kTest.end(), 0u, Gcd);
 
 void Run(benchmark::State& state) {
