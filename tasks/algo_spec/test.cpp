9fcdb6a6878d5cc3f97c979f1860418dad8e68b3
Andrey Chulkov <achulkov2@gmail.com>
2021-11-11 01:28:32 +0300

    better tests for algo_spec

--- tasks/algo_spec/test.cpp
+++ tasks/algo_spec/test.cpp
@@ -12,18 +12,28 @@ TEST_CASE("Integral") {
     REQUIRE(Check(-200, ComputeIntegral));
     REQUIRE(Check(40, ComputeIntegral));
     REQUIRE(Check(0, ComputeIntegral));
+    REQUIRE(Check(50ll, ComputeIntegral));
+    REQUIRE(Check(50l, ComputeIntegral));
+    REQUIRE(Check(static_cast<char>(2), ComputeIntegral));
+    REQUIRE(Check(static_cast<short int>(2), ComputeIntegral));
 }
 
 TEST_CASE("Unsigned integral") {
     REQUIRE(Check(0u, ComputeUnsignedIntegral));
     REQUIRE(Check(50u, ComputeUnsignedIntegral));
     REQUIRE(Check(-1u, ComputeUnsignedIntegral));
+    REQUIRE(Check(50ull, ComputeUnsignedIntegral));
+    REQUIRE(Check(50ul, ComputeUnsignedIntegral));
+    REQUIRE(Check(static_cast<unsigned char>(2), ComputeUnsignedIntegral));
+    REQUIRE(Check(static_cast<unsigned short int>(2), ComputeUnsignedIntegral));
 }
 
 TEST_CASE("Floating point") {
     REQUIRE(Check(0.0f, ComputeFloat));
     REQUIRE(Check(-3.1415926, ComputeFloat));
     REQUIRE(Check(NAN, ComputeFloat));
+    REQUIRE(Check(static_cast<double>(1.0), ComputeFloat));
+    REQUIRE(Check(static_cast<long double>(1.0), ComputeFloat));
 }
 
 TEST_CASE("General algo") {
