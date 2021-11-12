6783673d8c39c3821f2729772a24a98556b1f3dc
Andrey Chulkov <achulkov2@gmail.com>
2021-11-12 03:07:49 +0300

    small improvement to scheme_advanced pair mutation tests

--- tasks/scheme/advanced/test_pair_mut.cpp
+++ tasks/scheme/advanced/test_pair_mut.cpp
@@ -8,6 +8,14 @@ TEST_CASE_METHOD(SchemeTest, "PairMutations") {
 
     ExpectNoError("(set-cdr! x 6)");
     ExpectEq("(cdr x)", "6");
+
+    ExpectNoError("(define z 1543)");
+
+    ExpectNoError("(set-cdr! x z)");
+    ExpectEq("(cdr x)", "1543");
+
+    ExpectNoError("(set-car! x z)");
+    ExpectEq("(car x)", "1543");
 }
 
 TEST_CASE_METHOD(SchemeTest, "SelfReferenceCar") {
@@ -17,6 +25,9 @@ TEST_CASE_METHOD(SchemeTest, "SelfReferenceCar") {
 
     ExpectNoError("(set-car! (car x) (car x))");
     ExpectEq("(cdr x)", "2");
+
+    ExpectNoError("(set-cdr! (car (car x)) 1543)");
+    ExpectEq("(cdr x)", "1543");
 
     ExpectNoError("(set-car! (car (car x)) 3)");
     ExpectEq("(car x)", "3");
@@ -29,6 +40,9 @@ TEST_CASE_METHOD(SchemeTest, "SelfReferenceCdr") {
 
     ExpectNoError("(set-cdr! (cdr y) (cdr y))");
     ExpectEq("(car y)", "1");
+
+    ExpectNoError("(set-car! (cdr (cdr y)) 1543)");
+    ExpectEq("(car y)", "1543");
 
     ExpectNoError("(set-cdr! (cdr (cdr y)) 3)");
     ExpectEq("(cdr y)", "3");
