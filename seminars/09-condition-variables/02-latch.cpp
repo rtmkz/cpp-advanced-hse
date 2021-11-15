9467675d45f8e3a37617cd089139382b71833044
BigRedEye <mail@bigredeye.me>
2021-11-15 19:28:00 +0300

    feat: Add text for seminar 9

--- seminars/09-condition-variables/02-latch.cpp
+++ seminars/09-condition-variables/02-latch.cpp
@@ -1,9 +1,14 @@
+#include <algorithm>
 #include <chrono>
+#include <cmath>
 #include <condition_variable>
+#include <cstddef>
 #include <iostream>
 #include <mutex>
+#include <numeric>
 #include <thread>
 #include <vector>
+#include <deque>
 
 
 class Latch {
@@ -23,7 +28,7 @@ public:
     void Wait() {
         std::unique_lock lock{mtx_};
         cv_.wait(lock, [this] {
-            return value_ > 0;
+            return value_ == 0;
         });
     }
 
@@ -33,19 +38,60 @@ private:
     int value_ = 0;
 };
 
-int main() {
-    int num_threads = 10;
-    Latch latch{num_threads};
+////////////////////////////////////////////////////////////////////////////////
 
-    std::vector<std::jthread> workers;
-    for (int i = 0; i < num_threads; ++i) {
-        workers.emplace_back([&latch] {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            latch.Arrive();
-        });
+struct Bucket {
+    Bucket(size_t num_threads)
+        : sum_by_thread(num_threads, 0.0)
+        , latch(num_threads)
+    {
     }
 
-    std::cerr << "Waiting " << num_threads << " threads...\n";
-    latch.Wait();
-    std::cerr << "Done!\n";
+    std::vector<double> sum_by_thread;
+    Latch latch;
+};
+
+struct State {
+    State(size_t num_threads)
+        : num_threads{num_threads}
+    {
+        size_t num_buckets = limit / bucket_size;
+        for (size_t i = 0; i < num_buckets; ++i) {
+            buckets.emplace_back(num_threads);
+        }
+    }
+
+    size_t limit = 1'000'000'000;
+    size_t num_threads = 0;
+    size_t bucket_size = 100'000'000;
+    std::deque<Bucket> buckets;
+};
+
+void Worker(State& state, size_t thread_index) {
+    size_t prev_bucket = 0;
+    for (size_t num = thread_index; num < state.limit; num += state.num_threads) {
+        size_t bucket = num / state.bucket_size;
+        if (bucket > prev_bucket) {
+            state.buckets[prev_bucket].latch.Arrive();
+            prev_bucket = bucket;
+        }
+        state.buckets[bucket].sum_by_thread[thread_index] += std::sqrt(num);
+    }
+    state.buckets[prev_bucket].latch.Arrive();
+}
+
+int main() {
+    State state{std::thread::hardware_concurrency()};
+    std::vector<std::jthread> workers;
+    for (size_t i = 0; i < state.num_threads; ++i) {
+        workers.emplace_back(Worker, std::ref(state), i);
+    }
+
+    double sum = 0.0;
+    for (auto&& bucket : state.buckets) {
+        bucket.latch.Wait();
+        double delta = std::accumulate(bucket.sum_by_thread.begin(), bucket.sum_by_thread.end(), 0.0);
+        sum += delta;
+        std::cout << "Current sum: " << sum << ", delta: " << delta << std::endl;
+    }
 }
