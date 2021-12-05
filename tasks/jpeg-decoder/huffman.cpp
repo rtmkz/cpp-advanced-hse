c4b42119c4e9aa6d214724b154c26936873309b1
Tagir Kuskarov <kuskarovtf@mail.ru>
2021-12-05 13:59:25 +0300

    Suppress unused errors

--- tasks/jpeg-decoder/huffman.cpp
+++ tasks/jpeg-decoder/huffman.cpp
@@ -2,8 +2,13 @@
 
 void HuffmanTree::Build(const std::vector<uint8_t> &code_lengths,
                         const std::vector<uint8_t> &values) {
+    (void) code_lengths;
+    (void) values;
 }
 
 bool HuffmanTree::Move(bool bit, int &value) {
+    (void) bit;
+    (void) value;
+
     return true;
 }
