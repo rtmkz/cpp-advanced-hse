8d1b31a70d92d03fc140bac293154b39c8963010
Danila Kutenin <kutdanila@yandex.ru>
2021-12-08 11:58:02 +0000

    Make 8 seconds for faster

--- tasks/jpeg-decoder/faster/test_faster.cpp
+++ tasks/jpeg-decoder/faster/test_faster.cpp
@@ -11,7 +11,7 @@ TEST_CASE("huge", "[jpg]") {
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     CheckImage("huge.jpg");
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    REQUIRE(std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() < 6);
+    REQUIRE(std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() <= 7);
 #else
     std::cerr
         << "WARNING!: Build in release mode to test time, use -DCMAKE_BUILD_TYPE=RelWithDebInfo"
