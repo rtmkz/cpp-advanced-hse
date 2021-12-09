86ec4c3f028fac88354bda03f9c7a3270ec298a5
Tagir Kuskarov <kuskarovtf@mail.ru>
2021-12-09 22:01:06 +0300

    Still debugging

--- run_linter.sh
+++ run_linter.sh
@@ -30,7 +30,7 @@ set -x
 
 echo "Sources to be checked by clang-tidy:"
 
-jq -r '.allow_change | if type=="array" then .[] else . end' --raw-output $TASK_PATH/.tester.json | sed "s|^|$TASK_PATH\/|g" | egrep '\.c$|\.cpp$|\.h$|\.hpp$' | xargs -I@ sh -c "ls @"
+jq -r '.allow_change | if type=="array" then .[] else . end' --raw-output $TASK_PATH/.tester.json | sed "s|^|$TASK_PATH\/|g" | egrep '\.c$|\.cpp$|\.h$|\.hpp$' | xargs -I@ sh -c "ls @" | xargs stat
 
 if [ "$#" -eq 3 ]; then
     jq -r '.allow_change | if type=="array" then .[] else . end' --raw-output $TASK_PATH/.tester.json | sed "s|^|$TASK_PATH\/|g" | egrep '\.c$|\.cpp$|\.h$|\.hpp$' | xargs -I@ sh -c "ls @" | xargs $CLANG_TIDY --config="$3"
