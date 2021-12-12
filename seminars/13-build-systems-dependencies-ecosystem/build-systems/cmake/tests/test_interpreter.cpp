a0bcd098709c6ecd1ed9af50c21e94cc746b9141
TmLev <sololev27@gmail.com>
2021-12-12 22:22:54 +0300

    feat(seminars-13): Add `cmake` example

---
+++ seminars/13-build-systems-dependencies-ecosystem/build-systems/cmake/tests/test_interpreter.cpp
@@ -0,0 +1,14 @@
+#include <racket/interpreter.hpp>
+
+#include <cassert>
+#include <iostream>
+
+auto main() -> int {
+    auto interpreter = racket::Interpreter{};
+
+    {
+        assert(interpreter.Run("Expression") == "");
+    }
+
+    std::cout << "Tests passed" << std::endl;
+}
