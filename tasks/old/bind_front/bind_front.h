66523c674a5a40053b57abf80e22dc1ad4110086
Tagir Kuskarov <kuskarovtf@mail.ru>
2022-08-27 23:19:13 +0300

    Zdes nichego net

--- tasks/old/bind_front/bind_front.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <functional>
-#include <utility>
-
-template <class F, class... Args>
-constexpr auto BindFront(F&& f, Args&&... args) {
-    return f;  ///
-}
--- tasks/old/pipes/pipes.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <utility>
-
-// Your solution here
