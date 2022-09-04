31fdbca31170f336f21b056ff5bc08430fb7ed3f
Tagir Kuskarov <kuskarovtf@mail.ru>
2022-09-04 22:58:55 +0000

    Build seminar-1 examples

--- cmake/SeminarUtils.cmake
+++ cmake/SeminarUtils.cmake
@@ -1,10 +1,12 @@
 function(build_seminar_examples SEMINAR_DIR)
   message(STATUS "Building examples for seminar ${SEMINAR_DIR}...")
 
-  file(GLOB CPPS "${CMAKE_SOURCE_DIR}/seminars/${SEMINAR_DIR}/*.cpp")
+  file(GLOB CPPS "${CMAKE_SOURCE_DIR}/seminars/2022/${SEMINAR_DIR}/*.cpp")
 
   foreach(CPP IN LISTS CPPS)
     get_filename_component(FILENAME ${CPP} NAME_WE)
-    add_executable("seminars-${SEMINAR_DIR}-${FILENAME}" ${CPP})
+    set(TARGET_NAME "seminars-${SEMINAR_DIR}-${FILENAME}")
+    message(STATUS "\tAdding target ${TARGET_NAME}")
+    add_executable(${TARGET_NAME} ${CPP})
   endforeach()
 endfunction()
