badd96d8720c38b98d271c41dcebaae3f3f766f8
Tagir Kuskarov <kuskarovtf@mail.ru>
2022-12-02 21:21:43 +0000

    Add clang-fun

---
+++ tasks/bonus/clang-fun/run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+S=$(pwd)
+B=$S/../build/relwithdebinfo
+# B=$S/../build
+
+$B/check-names $S/tests/no-dict/*.cpp $S/check_names.cpp -p $B >/tmp/no-dict-result.txt 2>/dev/null
+$B/check-names $S/tests/dict/*.cpp -p $B -dict $S/tests/dict/dict.txt >/tmp/dict-result.txt 2>/dev/null
+
+diff $S/tests/no-dict/result.txt /tmp/no-dict-result.txt
+diff $S/tests/dict/result.txt /tmp/dict-result.txt
