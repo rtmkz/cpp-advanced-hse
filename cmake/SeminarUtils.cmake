90015873d486c7db543012547f04e396e3978eab
Tagir Kuskarov <kuskarovtf@mail.ru>
2023-09-14 21:39:33 +0000

    Fix seminars build

--- cmake/SeminarUtils.cmake
+++ cmake/SeminarUtils.cmake
@@ -5,7 +5,7 @@ function(build_seminar_examples SEMINAR_DIR)
   add_custom_target(${SEMINAR_TARGET_NAME})
   message(STATUS "Common target for this seminar: ${SEMINAR_TARGET_NAME}")
 
-  file(GLOB CPPS "${CMAKE_SOURCE_DIR}/seminars/2022/${SEMINAR_DIR}/*.cpp")
+  file(GLOB CPPS "${CMAKE_SOURCE_DIR}/seminars/2023/${SEMINAR_DIR}/*.cpp")
 
   foreach(CPP IN LISTS CPPS)
     get_filename_component(FILENAME ${CPP} NAME_WE)
