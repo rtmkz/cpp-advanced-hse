b9422013bc8559945a7122781a5274839682142f
rtmkz <zhan.kurbanbayev@gmail.com>
2024-02-21 03:31:07 +0600

    solve lru_cache

--- tasks/memory/lru-cache/lru_cache.h
+++ tasks/memory/lru-cache/lru_cache.h
@@ -1,6 +1,18 @@
 #pragma once
 
+#include <iostream>
 #include <string>
+#include <unordered_map>
+#include <memory>
+
+struct Node {
+    std::string key, value;
+    std::shared_ptr<Node> prev, next;
+    Node() : prev(nullptr), next(nullptr) {
+    }
+    Node(std::string key, std::string value) : key(key), value(value), prev(nullptr), next(nullptr) {
+    }
+};
 
 class LruCache {
 public:
@@ -10,5 +22,18 @@ public:
 
     bool Get(const std::string& key, std::string* value);
 
+    void Update(const std::string& key);
+
+    void Pop();
+
+    void Print() {
+        for (auto it : pos) {
+            std::cout << it.first << ' ' << it.second -> value << '\n';
+        }
+        std::cout << '\n';
+    }
 private:
+    std::shared_ptr<Node> head, tail;
+    size_t capacity;
+    std::unordered_map <std::string, std::shared_ptr<Node>> pos;
 };
