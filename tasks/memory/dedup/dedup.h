e80f9849917de1d7280c72eea15f7881afa7d441
rtmkz <zhan.kurbanbayev@gmail.com>
2024-02-20 23:58:19 +0600

    solve memory/dedup

--- tasks/memory/dedup/dedup.h
+++ tasks/memory/dedup/dedup.h
@@ -9,18 +9,20 @@ using std::string;
 
 std::vector<std::unique_ptr<string>> Duplicate(const std::vector<std::shared_ptr<string>>& items) {
     std::vector<std::unique_ptr<string>> out;
-    out.reserve(items.size()); 
+    out.reserve(items.size());
     for (auto& it : items) {
         out.emplace_back(std::make_unique<string>(*it));
     }
     return out;
 }
 
-std::vector<std::shared_ptr<string>> DeDuplicate(const std::vector<std::unique_ptr<string>>& items) {
+std::vector<std::shared_ptr<string>> DeDuplicate(
+    const std::vector<std::unique_ptr<string>>& items) {
     std::vector<std::shared_ptr<string>> out;
-    std::unordered_map <string, std::shared_ptr<string>> seen;
+    std::unordered_map<string, std::shared_ptr<string>> seen;
     for (auto& it : items) {
-        if (!seen.count(*it)) seen[*it] = std::make_shared<string>(*it);
+        if (!seen.count(*it))
+            seen[*it] = std::make_shared<string>(*it);
         out.emplace_back(seen[*it]);
     }
     return out;
