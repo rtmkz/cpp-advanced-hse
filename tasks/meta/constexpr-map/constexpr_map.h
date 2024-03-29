546f58de86112a982470e4fe6769e82029d1d0c7
rtmkz <zhan.kurbanbayev@gmail.com>
2024-03-30 02:18:22 +0500

    solved constexp-map

--- tasks/meta/constexpr-map/constexpr_map.h
+++ tasks/meta/constexpr-map/constexpr_map.h
@@ -1,28 +1,77 @@
 #pragma once
 
+#include <vector>
+
 template<class K, class V, int MaxSize = 8>
 class ConstexprMap {
 public:
-    constexpr ConstexprMap() = default;
+    constexpr ConstexprMap() {
+        size_ = 0;
+    }
 
     constexpr V& operator[](const K& key) {
+        for (size_t i = 0; i < size_; i++) {
+            if (arr_[i].first == key) {
+                return arr_[i].second;
+            }
+        }
+        if (size_ == MaxSize) {
+            throw std::runtime_error("Key not found!");
+        }
+        arr_[size_].first = key;
+        return arr_[size_++].second;
     }
 
     constexpr const V& operator[](const K& key) const {
+        for (size_t i = 0; i < size_; i++) {
+            if (arr_[i].first == key) {
+                return arr_[i].second;
+            }
+        }
+        throw std::runtime_error("Key not found!");
     }
 
     constexpr bool Erase(const K& key) {
+        size_t pos = size_;
+        for (size_t i = 0; i < size_; i++) {
+            if (arr_[i].first == key) {
+                pos = i;
+                break;
+            }
+        }
+        if (pos < size_) {
+            while (pos + 1 < size_) {
+                std::swap(arr_[pos].first, arr_[pos + 1].first);
+                std::swap(arr_[pos].second, arr_[pos + 1].second);
+                pos++;
+            }
+            size_--;
+            return true;
+        }
+        return false;
     }
 
     constexpr bool Find(const K& key) const {
+        for (size_t i = 0; i < Size(); i++) {
+            if (arr_[i].first == key) {
+                return true;
+            }
+        }
+        return false;
     }
 
     constexpr size_t Size() const {
+        return size_;
     }
 
     constexpr std::pair<K, V>& GetByIndex(size_t pos) {
+        return arr_[pos];
     }
 
     constexpr const std::pair<K, V>& GetByIndex(size_t pos) const {
+        return arr_[pos];
     }
+private:
+    std::array <std::pair<K, V>, MaxSize> arr_;
+    size_t size_;
 };
--- tasks/meta/constexpr-map/sort.h
+++ tasks/meta/constexpr-map/sort.h
@@ -4,6 +4,34 @@
 
 #include <type_traits>
 
+template<class K, class V>
+constexpr void Swap(std::pair<K, V>& a, std::pair<K, V>& b, bool f = 0) {
+	if (!f) {
+		// increasing
+		if (b < a) {
+			std::swap(a.first, b.first);
+			std::swap(a.second, b.second);
+		}
+	} 
+	else {
+		// decreasing
+		if (a < b) {
+			std::swap(a.first, b.first);
+			std::swap(a.second, b.second);
+		}
+	}
+}
+
 template<class K, class V, int S>
 constexpr auto Sort(ConstexprMap<K, V, S> map) {
+	bool f = 0;
+	for (size_t i = 0; i < map.Size(); i++) {
+		f |= std::is_integral_v<K>;
+	}
+	for (size_t i = 0; i < map.Size(); i++) {
+		for (size_t j = i + 1; j < map.Size(); j++) {
+			Swap(map.GetByIndex(i), map.GetByIndex(j), f);
+		}
+	}
+	return map;
 }
