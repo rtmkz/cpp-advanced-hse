7fc09c2965372112306fd7bd4eeebf38f4e18733
rtmkz <zhan.kurbanbayev@gmail.com>
2024-03-29 22:40:19 +0500

    solved pow, sqrt, another_pow, hash, next_prime

--- tasks/meta/compile-eval/hash.h
+++ tasks/meta/compile-eval/hash.h
@@ -1,5 +1,9 @@
 #pragma once
 
+
+constexpr int hashCalc(const char *s, int p, int pw, int mod) {
+    return (s[0] == '\0') ? 0 : (hashCalc(s + 1, p, (pw * 1LL * p % mod), mod) + s[0] * 1LL * pw) % mod;
+}
 constexpr int hash(const char *s, int p, int mod) {
-    return 0;
+    return hashCalc(s, p, 1, mod);
 }
--- tasks/meta/compile-eval/sqrt.h
+++ tasks/meta/compile-eval/sqrt.h
@@ -1,4 +1,26 @@
+template<bool f, int a, int b>
+struct Condition {
+    static const int value = a;
+};
+template<int a, int b>
+struct Condition<false, a, b> {
+    static const int value = b;
+};
+
+template<int N, int l, int r>
+struct Bs {
+    static const long long int mid = (l + r) / 2;
+    static const int nextL = Condition<(mid * mid < N), mid + 1, l>::value;
+    static const int nextR = Condition<(mid * mid < N), r, mid>::value;
+    static const int value = Bs<N, nextL, nextR>::value;
+};
+
+template<int N, int l>
+struct Bs<N, l, l> {
+    static const int value = l;
+};
+
 template<int N>
 struct Sqrt {
-    static const int value = 1;
+    static const int value = Bs<N, 1, N>::value;
 };
