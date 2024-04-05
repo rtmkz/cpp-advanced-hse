d49b11c204f3ce716baf64ec13d0a851905b00ee
rtmkz <zhan.kurbanbayev@gmail.com>
2024-04-05 19:25:59 +0500

    solve string_view

--- tasks/memory/string-view/string_view.h
+++ tasks/memory/string-view/string_view.h
@@ -1,5 +1,32 @@
 #pragma once
 
 #include <string>
+#include <algorithm>
+#include <cstring>
 
-class StringView {};
+class StringView {
+
+public:
+    StringView(const std::string& s, size_t start = 0, size_t len = std::string::npos) {
+        head = &s.c_str()[start];
+        size_ = std::min(len, s.size() - start);
+    }
+    StringView(const char* s) {
+        head = s;
+        size_ = strlen(s);
+    }
+    StringView(const char* s, size_t len) {
+        head = s;
+        size_ = len;
+    }
+    char operator[](int i) const {
+        return *(head + i);
+    }
+    size_t Size() const {
+        return size_;
+    }
+    
+private:
+    const char* head;
+	size_t size_;
+};
